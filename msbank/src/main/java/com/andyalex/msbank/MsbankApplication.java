78e7c933d9c8f88c8dd34c950d5de6f8f5693278
joellokota@gmail.com <joellokota@gmail.com>
2025-09-10 03:45:57 +0200

    first commit

---
+++ msbank/src/main/java/com/andyalex/msbank/MsbankApplication.java
@@ -0,0 +1,13 @@
+package com.andyalex.msbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsbankApplication.class, args);
+    }
+
+}
---
+++ msbank/src/test/java/com/andyalex/msbank/MsbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andyalex.msbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
